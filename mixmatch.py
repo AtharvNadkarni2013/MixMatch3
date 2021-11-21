5bd5a7edc42c38800ae01c125e238a9b6133be5c
Atharv Nadkarni <81621785+AtharvNadkarni2013@users.noreply.github.com>
2021-11-21 11:10:31 +0530

    Reformat file

--- mixmatch.py
+++ mixmatch.py
@@ -1,31 +1,34 @@
 import enum
 
+
 class Colors(enum.Enum):
-  red = enum.auto()
-  yellow = enum.auto()
-  blue = enum.auto()
-  green = enum.auto()
-  orange = enum.auto()
-  purple = enum.auto()
-  empty = enum.auto()
-  
+    red = enum.auto()
+    yellow = enum.auto()
+    blue = enum.auto()
+    green = enum.auto()
+    orange = enum.auto()
+    purple = enum.auto()
+    empty = enum.auto()
+
+
 red_transitions = {
-  Colors.red: Colors.red,
-  Colors.yellow: Colors.orange,
-  Colors.blue: Colors.purple
+    Colors.red: Colors.red,
+    Colors.yellow: Colors.orange,
+    Colors.blue: Colors.purple
 }
 
 yellow_transitions = {
-  Colors.red: Colors.orange,
-  Colors.yellow: Colors.yellow,
-  Colors.blue: Colors.green
+    Colors.red: Colors.orange,
+    Colors.yellow: Colors.yellow,
+    Colors.blue: Colors.green
 }
 
 blue_transitions = {
-  Colors.red: Colors.purple,
-  Colors.yellow: Colors.green,
-  Colors.blue: Colors.blue
+    Colors.red: Colors.purple,
+    Colors.yellow: Colors.green,
+    Colors.blue: Colors.blue
 }
+
 
 def mix_red(s: Colors) -> Colors:
     return red_transitions[s]
@@ -33,8 +36,8 @@ def mix_red(s: Colors) -> Colors:
 
 def mix_yellow(s: Colors) -> Colors:
     return yellow_transitions[s]
-    
-    
+
+
 def mix_blue(s: Colors) -> Colors:
     return blue_transitions[s]
 
@@ -51,11 +54,12 @@ def mix_many(s: Colors, mixes: str) -> Colors:
         else:
             raise ValueError('invalid mix')
     return s
-    
+
+
 def test_many_slaps():
-  assert mix_many(Colors.empty, 'rr') is Colors.red
-  assert mix_many(Colors.empty, 'yy') is Colors.yellow
-  assert mix_many(Colors.empty, 'bb') is Colors.blue
-  assert mix_many(Colors.empty, 'ry') is Colors.orange
-  assert mix_many(Colors.empty, 'rb') is Colors.green
-  assert mix_many(Colors.empty, 'br') is Colors.purple
+    assert mix_many(Colors.empty, 'rr') is Colors.red
+    assert mix_many(Colors.empty, 'yy') is Colors.yellow
+    assert mix_many(Colors.empty, 'bb') is Colors.blue
+    assert mix_many(Colors.empty, 'ry') is Colors.orange
+    assert mix_many(Colors.empty, 'rb') is Colors.green
+    assert mix_many(Colors.empty, 'br') is Colors.purple
